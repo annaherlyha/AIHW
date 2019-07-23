54e85a092daa12d1f22f4b93be3fdbf391f93ff4
Anna_Herlyha <Anna_Herlyha@epam.com>
2019-07-23 12:16:35 +0300

    update logger

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -1,4 +1,8 @@
 import logging.config
+from os import path
 
-logging.config.fileConfig('logging.conf')
+file_dir = path.dirname(path.abspath(__file__))
+log_file_path = path.join(file_dir, 'logging.ini')
+
+logging.config.fileConfig(log_file_path)
 logger = logging.getLogger(__name__)
